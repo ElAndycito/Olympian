a4f7d7efc43e0f444039dd6950c4c7dcb4551988
Manuel <manuelantoniopedrazarojas@gmail.com>
2023-11-16 21:07:38 -0500

    canciones r

--- app/src/main/java/com/example/olympian/MainActivity.java
+++ app/src/main/java/com/example/olympian/MainActivity.java
@@ -21,6 +21,9 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import android.media.MediaPlayer;
+import android.media.AudioAttributes;
+import java.io.IOException;
 
 
 
@@ -35,6 +38,8 @@ public class MainActivity extends AppCompatActivity{
     private StorageReference storageRef;
     private TextView textotitulo;
     private boolean isPlaying = false;
+    private MediaPlayer mediaPlayer;
+    private int currentSongIndex = 0;
 
 
 
@@ -50,19 +55,63 @@ public class MainActivity extends AppCompatActivity{
         textotitulo = findViewById(R.id.txtmusictitulo);
         storage = FirebaseStorage.getInstance();
         storageRef = storage.getReference();
+        textotitulo.setText("Another one bite the dust");
+        String[] urlsCanciones = {
+                "android.resource://" + getPackageName() + "/" + R.raw.another_one_bites_the_dust,
+                "android.resource://" + getPackageName() + "/" + R.raw.eye_of_the_tiger,
+                "android.resource://" + getPackageName() + "/" + R.raw.i_want_to_break_free,
+                "android.resource://" + getPackageName() + "/" + R.raw.mi_gente,
+                "android.resource://" + getPackageName() + "/" + R.raw.somebody_to_love,
+                "android.resource://" + getPackageName() + "/" + R.raw.till_i_collapse,
+                "android.resource://" + getPackageName() + "/" + R.raw.under_pressure
+        };
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build());
+        try {
+            mediaPlayer.setDataSource(urlsCanciones[0]);
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        btnPausarmusica.setBackgroundResource(R.drawable.unpause);
+        btnAdelantemusica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentSongIndex++;
+                if (currentSongIndex >= urlsCanciones.length) {
+                    currentSongIndex = 0; // Volver al inicio del array
+                }
+                cambiarCancion(urlsCanciones[currentSongIndex]);
+            }
+        });
+        btnAtrasmusica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentSongIndex--;
+                if (currentSongIndex < 0) {
+                    currentSongIndex = urlsCanciones.length - 1;
+                }
+                cambiarCancion(urlsCanciones[currentSongIndex]);
+            }
+        });
         btnPausarmusica.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (isPlaying) {
-                    // Cambiar el icono a "ic_media_play"
-                    //btnPausarmusica.setBackgroundResource(R.drawable.ic_media_play);
+                    // Cambiar el icono a "pausar.png"
+                    btnPausarmusica.setBackgroundResource(R.drawable.pausar);
+                    mediaPlayer.pause();
                     isPlaying = false;
                 } else {
-                    // Cambiar el icono a "ic_media_pause"
-                    //btnPausarmusica.setBackgroundResource(R.drawable.ic_media_pause);
+                    // Cambiar el icono a otro recurso de imagen
+                    btnPausarmusica.setBackgroundResource(R.drawable.unpause);
+                    mediaPlayer.start();
                     isPlaying = true;
                 }
-            }//BORRAR ESTE TILÍN
+            }
         });
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -112,6 +161,38 @@ public class MainActivity extends AppCompatActivity{
                         Log.d("Firestore", "Error getting documents: ", task.getException());
                     }
                 });
+    }
+    private void cambiarCancion(String urlCancion) {
+        try {
+            if(currentSongIndex==0){
+                textotitulo.setText("Another one bite the dust");
+            }
+            if(currentSongIndex==1){
+                textotitulo.setText("Eye of the tiger");
+            }
+            if(currentSongIndex==2){
+                textotitulo.setText("I want to break free");
+            }
+            if(currentSongIndex==3){
+                textotitulo.setText("Mi gente");
+            }
+            if(currentSongIndex==4){
+                textotitulo.setText("Somebody to love");
+            }
+            if(currentSongIndex==5){
+                textotitulo.setText("Till I collapse");
+            }
+            if(currentSongIndex==6){
+                textotitulo.setText("Under pressure");
+            }
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(urlCancion);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            isPlaying = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
